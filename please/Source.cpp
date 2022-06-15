d1f73fff5a771219b76c0b0ab4c67d7435368cc5
Ryan <sturgeryan@myvuw.ac.nz>
2022-06-15 22:04:08 +1200

    doesn't have app struct in it... check app.h or app.cpp

--- please/Source.cpp
+++ please/Source.cpp
@@ -1,57 +1,12 @@
-#include <fstream>
 #include <ctime>
-#include "user.cpp"
-#include "actions.h"
-#include <string>
-#include <cmath>
-#include <iomanip>
-#include <sstream>
+#include "app.h"
+#include <iostream>
 
 using namespace std;
 
-string path = "C:\\Users\\Ry-Bread\\Documents\\data.txt";
-
-
-
-struct app {
-    User activePerson;
-    void readAllLines(){
-        ifstream f; f.open(path); string str; vector<string> contents;
-        while(f.good()){
-            while(getline(f,str)){
-                contents.push_back(str);
-            }
-        }
-        f.close();
-
-        activePerson.name = contents.at(0);
-        int i = 0;
-        while(i < stoi(contents.at(1))) {
-            activePerson.accounts.push_back(stod(contents.at(2 + i)));
-            i++;
-        }
-        activePerson.lastLine = contents.size();
-        activePerson.transcationStart = 2 + i;
-    }
-    void printLines() {
-        int num = activePerson.accounts.size();
-        string str = "Name: " + activePerson.name + "\nNumber of accounts: " 
-            + to_string(num);
-        for (int i = 0; i < num; i++) {
-            stringstream stream;
-            double value = activePerson.accounts.at(i);
-            stream << fixed << setprecision(2) << value;
-            string str1 = "\nAccount " + to_string(i+1) + ": " + stream.str();
-            str.append(str1);
-        }
-        cout << str << endl;
-    }
-};
-
-
 int main() {
 
-    app main;
+    App main;
 
     time_t now = time(NULL);
 
@@ -61,8 +16,8 @@ int main() {
 
     //cout << "Happy " << nowLocal.tm_year + 1900 << endl;
     
-    main.readAllLines();
-    main.printLines();
+    main.storePerson();
+    main.printDefault();
 
     return 0;
 }
