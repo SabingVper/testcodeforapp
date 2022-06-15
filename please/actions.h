fbf6d21174ccb26b5a1b7caf32832c00845973b6
Ryan <sturgeryan@myvuw.ac.nz>
2022-06-15 22:01:30 +1200

    Proper header file for actions

--- please/actions.h
+++ please/actions.h
@@ -1,33 +1,32 @@
 #pragma once
-#ifndef actions_h
-#define actions_h
+#ifndef actions_h_
+#include <string>
+#include <vector>
 
-struct Action {
-    int date[3]; // Stores Day/Month/Year
-    double amount; // Stores Amount
-    char action; // Stores action "T" == Transfer, "A" == Add, "R" == Remove
-    int accountTo; // Stores where the money is going to
-    int accountFrom; // Stores where the money is coming from
-    std::string reason; // Stores a String of the comment
-
-    void store(int dmy[3], double amo, char act, int to, int from, std::string com) {
-        for (int i = 0; i < 3; i++) {
-            date[i] = dmy[i];
-        }
-        amount = amo;
-        action = act;
-        accountTo = to;
-        accountFrom = from;
-        reason = com;
-    }
-    /**
-        @title 
-
-    */
-    void print() {
-
-    }
+class Action {
+    public: 
+        void store(std::vector<int> dmy, double amo, char act, int to, int from, std::string com);
+        void savetoFile(std::string& path);
+        void print();
+        std::vector<int> getDate();
+        double getAmount();
+        char getAction();
+        int getAccountTo();
+        int getAccountFrom();
+        std::string getReason();
+        void setDate(int d);
+        void setAmount(double amo);
+        void setAction(char act);
+        void setAccountTo(int to);
+        void setAccountFrom(int from);
+        void setReason(std::string str);
+    private:
+        std::vector<int> date; // Stores Day/Month/Year
+        double amount; // Stores Amount
+        char action; // Stores action "T" == Transfer, "A" == Add, "R" == Remove
+        int accountTo; // Stores where the money is going to
+        int accountFrom; // Stores where the money is coming from
+        std::string reason; // Stores a String of the comment
 };
 
-
-#endif // !actions_h
+#endif // !actions_h_
